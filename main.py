5bbc698f229040ef775b92304115d796c5c850fc
天仙婆婆 <tqx5201@163.com>
2026-03-16 10:53:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -186,7 +186,7 @@ async def check_link_quality(session, link):
         async with session.head(link, timeout=timeout, allow_redirects=True) as response:
             response_time = time.time() - start_time
             if response.status == 200:
-                print(url)
+                print(link)
                 return response_time
             return float('inf')
     except:
